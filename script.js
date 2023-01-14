4fc7284731c8c002976f4dde46e7ae3a8fb93380
Enes <seyit7enes@gmail.com>
2023-01-14 16:07:35 +0100

    updating js

--- script.js
+++ script.js
@@ -2,15 +2,13 @@
 //select all the question cards
 const questions = document.querySelectorAll(".card");
 
-//select each of the cards
-questions.forEach(function (question) {
-  //select the button, answer, and icon of each card
-  const btn = question.querySelector(".icon");
-  const answer = question.querySelector(".answer");
-  const minusIcon = question.querySelector(".icon-minus");
+questions.forEach((card) => {
+  const btn = card.querySelector(".icon");
+  const answer = card.querySelector(".answer");
+  const minusIcon = card.querySelector(".icon-minus");
   // click event on each button
   btn.addEventListener("click", function () {
-    question.classList.toggle("taller");
+    card.classList.toggle("taller");
     btn.classList.toggle("rotate");
     minusIcon.classList.toggle("hide");
     answer.classList.toggle("hide");
