5798e70a8bfb53b33acdb8d8720958fe4ca85652
Seyit Enes <seyit7enes@gmail.com>
2023-04-13 19:01:06 +0200

    revisiting

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
 "use strict";
-//select all the question cards
+
+// Get all the question cards
 const questions = document.querySelectorAll(".card");
 
-questions.forEach((card) => {
+// Toggle the visibility of a card's answer and icon
+function toggleCard(card) {
   const btn = card.querySelector(".icon");
   const answer = card.querySelector(".answer");
   const minusIcon = card.querySelector(".icon-minus");
-  // click event on each button
-  btn.addEventListener("click", function () {
-    card.classList.toggle("taller");
-    btn.classList.toggle("rotate");
-    minusIcon.classList.toggle("hide");
-    answer.classList.toggle("hide");
-  });
+  card.classList.toggle("taller");
+  btn.classList.toggle("rotate");
+  minusIcon.classList.toggle("hide");
+  answer.classList.toggle("hide");
+}
+
+// Add click event listener to each button
+questions.forEach((card) => {
+  const btn = card.querySelector(".icon");
+  btn.addEventListener("click", () => toggleCard(card));
 });
